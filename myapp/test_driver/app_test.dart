23b6583722c556e63d340590f9fe8a70f8944672
Mahmood D Monkabadi <m.mahmooddm@gmail.com>
2021-02-25 15:14:28 +0330

    add app_test.dart with deafault configuratiion

---
+++ myapp/test_driver/app_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Testing App Performance Tests', () {
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+  });
+}
